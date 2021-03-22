e4298e9abd27667cb2a2b3c67214c9ed56ef8934
ardwang <278161009@qq.com>
2021-03-22 19:05:39 +0800

    update rnd 2021/3/22

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 布局代码
+import Element from 'element-ui'
+import "element-ui/lib/theme-chalk/index.css"
+
+// 网络请求
+import axios from 'axios'
+Vue.prototype.$axios = axios
+
+Vue.use(Element)
+
 Vue.config.productionTip = false
 
 new Vue({
